197363a9c3e1199b9b1d5fb08e819a6c1064b5a4
toolateralus <109556963+toolateralus@users.noreply.github.com>
2025-04-20 13:18:15 -0400

    feat: implement argument loading and function call handling in emitter

--- output.asm
+++ output.asm
@@ -13,6 +13,26 @@ div r11
 icmp rbx, 0 ; if (cond)
 je .cont0 ; if (!cond)
 .if0:
+mov r10, [rbp - 8] ; loading Interned { "a" }
+push rdi
+mov rdi, r10 ; load arg 0 -> 
+mov r10, rsi ; loading Interned { "b" }
+push rsi
+mov rsi, r10 ; load arg 1 -> 
+call printn
+pop rdi ; restore
+pop rsi ; restore
 .cont0:
+mov r10, rdi ; loading Interned { "n" }
+mov r11, rsi ; loading Interned { "b" }
+mov rax, r10
+cqo
+ r11
+mov r10, 0 ; loading literal
+add rdx, r10
+push rdi
+mov rdi, rdx ; load arg 0 -> 
+call putchar
+pop rdi ; restore
 ret
 
