b28b91613bf2d5067feff8c2606df5245aec7dcd
cooper <cooper.pilot@hotmail.com>
2025-04-20 13:15:21 -0400

    added assign

--- output.asm
+++ output.asm
@@ -3,14 +3,13 @@ printn:
 push rbp
 mov rbp, rsp
 sub rsp, 8 ; alloca
-mov rbx, [rbp - 8] ; loading Interned { "a" }
+mov rbx, rsi ; loading Interned { "b" }
 mov r10, rdi ; loading Interned { "n" }
-mov r11, rsi ; loading Interned { "b" }
 mov rax, r10
 cqo
-div r11
- rbx, rax
-icmp rbx, 0 ; if (cond)
+div rbx
+mov [rbp - 8], rax ; storing Interned { "a" }
+icmp 0, 0 ; if (cond)
 je .cont0 ; if (!cond)
 .if0:
 .cont0:
